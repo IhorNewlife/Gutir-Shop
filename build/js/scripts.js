b88d05af6be10d27299412ee2e5736315eb820ee
Igor <newlifeamf@gmail.com>
2022-08-24 20:45:27 +0300

    piy

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -1,3 +1,4 @@
+// Custom Scripts
 // Custom scripts
 // Мобильное меню бургер
 function burgerMenu() {
@@ -39,3 +40,4 @@ function fixedNav() {
   }
 }
 window.addEventListener("scroll", fixedNav);
+
